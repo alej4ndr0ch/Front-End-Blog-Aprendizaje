27484d5bc0cb311449a16493860a1d9e08a3d50a
JeremyArevalo1 <jarevalo-2023528@kinal.edu.gt>
2025-05-06 11:27:23 -0600

    blog personal actualizacion

---
+++ src/services/api.jsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: 'http://127.0.0.1:8080/twitch/v1',
+    timeout: 5000
+})
+
+apiClient.interceptors.request.use(
+    (config) => {
+        const useUserDetails = localStorage.getItem('user')
+
+        if(useUserDetails){
+            const token = JSON.parse(useUserDetails).token
+            config.headers.Authorization = `Bearer ${token}`
+        }
+
+        return config;
+    },
+    (e) => {
+        return Promise.reject(e)
+    }
+)
+
+export const login = async(data) => {
+    try {
+        return await apiClient.post('/auth/login', data)
+    } catch (e) {
+        return{
+            error: true,
+            e
+        }
+    }
+}
+
+export const register = async(data) => {
+    try {
+        return await apiClient.post('/auth/register', data)
+    } catch (e) {
+        return{
+            error: true,
+            e
+        }
+    }
+}
---
+++ src/shared/validators/index.jsx
@@ -0,0 +1,6 @@
+export * from './validateEmail.jsx';
+export * from './validatePassword.jsx';
+export * from './validateUsername.jsx';
+export * from './validateDescription.jsx';
+export * from './validateConfirPassword.jsx'
+export * from './validateTitle.jsx'
